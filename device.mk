833a39ed66cf216547a1fadef62015f4f2361e99
Razziell <razziell@ya.ru>
2018-06-20 22:47:04 +0300

    Add Xiaomidoze from MiracleDROID-HnT

--- device.mk
+++ device.mk
@@ -395,3 +395,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiDoze \
+    XiaomiParts
