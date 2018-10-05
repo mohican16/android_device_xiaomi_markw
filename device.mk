86b266e82482b3169dda132959c1d016be9234b1
Razziell <razziell@ya.ru>
2018-10-05 21:09:22 +0300

    msm8953-common: Add display hals

--- device.mk
+++ device.mk
@@ -163,6 +163,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
+PRODUCT_PACKAGES += \
+    vendor.display.color@1.0-service \
+    vendor.display.color@1.0-impl
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
